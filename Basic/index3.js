79f597a1f8c3d9c8f37efc6b7766d246f06da1a1
Jerry <rippleblue2002@gmail.com>
2015-07-12 15:20:58 +0800

    Basic
    Basic

---
+++ Basic/index3.js
@@ -0,0 +1,10 @@
+var server = require("./server5");
+var router = require("./router2");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.router,handle);
